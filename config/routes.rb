1b136135adfc3ea2b71e5000818bf5457a967e30
diwa bitchoka <bdiwa89@gmail.com>
2019-08-16 14:55:34 +0200

    new commit3

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :attendances
   resources :events
   resources :users
+  root 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
