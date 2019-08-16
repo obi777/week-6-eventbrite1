4316adcfcdb1cfa081f1fa9791f3a4a61ba89fd7
diwa bitchoka <bdiwa89@gmail.com>
2019-08-16 13:52:14 +0200

    new commit2

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,10 +4,15 @@ class Event < ApplicationRecord
   has_many :attendances
   has_many :participants, class_name: 'User', through: :attendances
 
-  validates :start_date, presence: true, :in_the_future?
+  validates :start_date, presence: true, start_date: true
   validates :duration, presence: true, duration: true
   validates :title, presence: true, length: { in: 5..140 }
   validates :description, presence: true, length: { in: 20..1000 }
   validates :price, presence: true, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 1000 }
   validates :location, presence: true
+
+
+  def end_date
+    self.start_date + self.duration * 60
+  end
 end
